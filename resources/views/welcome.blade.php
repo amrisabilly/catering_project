5853d9c7044a8014febd22417493a42da7f30f82
bilazahirah <bilazahirah.13@gmail.com>
2025-04-17 19:55:53 +0700

    tambahannavbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel + Tailwind</title>
+    <title>Catering</title>
     @vite('resources/css/app.css')
 </head>
 
@@ -12,24 +12,34 @@
 
     <div>
 
-        {{-- Navbar --}}
-        <div class="bg-slate-800 w-full h-14">
+                {{-- Navbar --}}
+                <div class="bg-[#FFFADD] w-full h-40 flex items-center justify-between px-6 rounded-b-lg shadow-md">
             
-            {{-- Logo --}}
-            <img src="" alt="">
-
-            {{-- List --}}
-            <ul>
-                <li>
-                    <a href="">Home</a>
-                    <a href="">Menu</a>
-                    <a href="">Order</a>
-                    <a href="">Order</a>
-                    <a href="">Order</a>
-                </li>
-            </ul>
-        </div>
-
+                    {{-- Logo --}}
+                    <img src="img/logocatering.png" alt="" class="w-[143px] h-[143px] margin">
+        
+                    {{-- List --}}
+                    <div class="flex items-center justify-center bg-[#FFFADD] rounded-[40px] shadow-md w-[523px] h-[50px]">
+                    <ul class="flex space-x-6 text-gray-800">
+                        <li>
+                            <a href="#" class="hover:bg-gray-200 hover:text-gray-900 px-3 py-2 rounded-lg transition duration-300">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:bg-gray-200 hover:text-gray-900 px-3 py-2 rounded-lg transition duration-300">Menu</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:bg-gray-200 hover:text-gray-900 px-3 py-2 rounded-lg transition duration-300">Order</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:bg-gray-200 hover:text-gray-900 px-3 py-2 rounded-lg transition duration-300">Contact</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:bg-gray-200 hover:text-gray-900 px-3 py-2 rounded-lg transition duration-300">About</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+        
     </div>
 </body>
 
